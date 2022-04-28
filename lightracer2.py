5fbcd25dad8679a36cb11db8c9b335a412be5894
David Fan <37087706+dmonfan@users.noreply.github.com>
2022-04-27 21:06:52 -0700

    Add files via upload

---
+++ lightracer2.py
@@ -0,0 +1,137 @@
+# Light Racer
+# By David Fan
+
+# Wormy (a Nibbles clone)
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Released under a "Simplified BSD" license
+
+import random, pygame, sys
+from pygame.locals import *
+
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+CELLSIZE = 20
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
+
+#             R    G    B
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+DARKGRAY  = ( 40,  40,  40)
+BGCOLOR = BLACK
+
+def main():
+    global CLOCK1, CLOCK2, DISPLAYSURF, BASICFONT
+
+    pygame.init()
+    CLOCK1 = pygame.time.Clock()
+    CLOCK2 = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    pygame.display.set_caption('Light Racer')
+
+    while True:
+        runGame()
+        showReplayScreen()
+
+
+def runGame():
+    timelastUpdate = 0
+    timelastUpdate2 = 0
+
+    lightCoord  = {'x': 5, 'y': 5}
+    lightCoord2 = {'x': 5, 'y': 10}
+
+    while True: # main game loop
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+
+        if lightCoord['x'] == -1 or lightCoord['x'] == CELLWIDTH or lightCoord['y'] == -1 or lightCoord['y'] == CELLHEIGHT:
+            return # game over
+
+        if lightCoord2['x'] == -1 or lightCoord2['x'] == CELLWIDTH or lightCoord2['y'] == -1 or lightCoord2['y'] == CELLHEIGHT:
+            return # game over
+
+        timelastUpdate = timelastUpdate + CLOCK1.get_time()
+        timelastUpdate2 = timelastUpdate2 + CLOCK2.get_time()
+        # print(f"Update 1 {timelastUpdate} Update 2 {timelastUpdate2}")
+
+        if timelastUpdate > 16:
+            DISPLAYSURF.fill(BGCOLOR)
+            drawGrid()
+            drawLight(lightCoord)
+            drawLight(lightCoord2)
+            lightCoord = {'x': lightCoord['x'] + 1, 'y': lightCoord['y']}
+            # print("Draw light 1")
+            timelastUpdate = 0
+
+        if timelastUpdate2 > 32:
+            DISPLAYSURF.fill(BGCOLOR)
+            drawGrid()
+            drawLight(lightCoord)
+            drawLight(lightCoord2)
+            lightCoord2 = {'x': lightCoord2['x'] + 1, 'y': lightCoord2['y']}
+            # print("Draw light 2")
+            timelastUpdate2 = 0
+
+        pygame.display.update()
+        CLOCK1.tick(64)
+        CLOCK2.tick(64)
+
+def drawPressKeyMsg():
+    pressKeySurf = BASICFONT.render('Press a key to play.', True, DARKGRAY)
+    pressKeyRect = pressKeySurf.get_rect()
+    pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
+    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+
+def checkForKeyPress():
+    if len(pygame.event.get(QUIT)) > 0:
+        terminate()
+
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def showReplayScreen():
+    drawPressKeyMsg()
+    pygame.display.update()
+    pygame.time.wait(500)
+    checkForKeyPress() # clear out any key presses in the event queue
+
+    while True:
+        if checkForKeyPress():
+            pygame.event.get() # clear event queue
+            return
+
+def drawLight(coord):
+    x = coord['x'] * CELLSIZE
+    y = coord['y'] * CELLSIZE
+    lightRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
+    pygame.draw.rect(DISPLAYSURF, RED, lightRect)
+
+
+def drawGrid():
+    for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
+    for y in range(0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+
+
+if __name__ == '__main__':
+    main()
